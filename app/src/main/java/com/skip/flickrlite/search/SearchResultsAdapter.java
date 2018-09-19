1baa8ccd1c39c2428088f3c9fef1d733f14998d6
David Tian <davidtian@Davids-MacBook-Pro.local>
2018-09-19 14:53:21 -0700

    wip fetching all images

--- app/src/main/java/com/skip/flickrlite/search/SearchResultsAdapter.java
+++ app/src/main/java/com/skip/flickrlite/search/SearchResultsAdapter.java
@@ -1,12 +1,15 @@
 package com.skip.flickrlite.search;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.skip.flickrlite.R;
@@ -26,12 +29,16 @@ class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.Vie
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.search_result_cell, viewGroup, false);
+
         return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        viewHolder.mTitle.setText(mData.get(i).mUrl);
+//        viewHolder.mTitle.setText(mData.get(i).mUrl);
+//        viewHolder.mTitle.setText("Downloading " + mData.get(i).mTitle); // TODO set progress bar
+        new DownloadImageTask(viewHolder).execute(mData.get(i).mUrl);
     }
 
     @Override
@@ -39,14 +46,24 @@ class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.Vie
         return mData.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements DownloadImageTask.OnCompleteCallback {
         private ImageView mContent;
-        private TextView mTitle;
+//        private TextView mTitle;
+        private ProgressBar mProgress;
         ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mContent = itemView.findViewById(R.id.search_result_cell_image_view);
-            mTitle = itemView.findViewById(R.id.search_result_cell_title_text_view);
+//            mTitle = itemView.findViewById(R.id.search_result_cell_title_text_view);
+            mProgress = itemView.findViewById(R.id.search_result_cell_progress_bar);
+        }
+
+        @Override
+        public void onComplete(Bitmap result) {
+            mContent.setImageBitmap(result);
+            mContent.setVisibility(View.VISIBLE);
+
+            mProgress.setVisibility(View.GONE);
         }
     }
 }
